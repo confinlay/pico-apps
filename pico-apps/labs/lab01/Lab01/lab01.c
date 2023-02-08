4c6cc6839d5dc397e4b084052afb6addcdd329f9
Conor Finlay <conorfin02@gmail.com>
2023-02-08 12:12:05 +0000

    initial commit

---
+++ pico-apps/labs/lab01/Lab01/lab01.c
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+/**
+ * @brief LAB #01 - TEMPLATE
+ *        Main entry point for the code.
+ * 
+ * @return int      Returns exit-status zero on completion.
+ */
+int main() {
+
+    // Returning zero indicates everything went okay.
+    return 0;
+}
